e25f8db78183fccd92832eb33a75966573ccdda5
danfucius <danfucius@gmail.com>
2017-02-10 15:19:33 -0800

    whitelist some shiz

--- app/app.js
+++ app/app.js
@@ -2,4 +2,8 @@
     'use strict';
 
     angular.module('app', ['app.directives', 'app.services']);
+
+    angular.module('app').config(function ($sceDelegateProvider) {
+        $sceDelegateProvider.resourceUrlWhitelist(['self', 'https://services.runescape.com/m=itemdb_oldschool/**']);
+    });
 })();
