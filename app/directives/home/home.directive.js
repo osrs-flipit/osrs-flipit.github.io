84784dcc453bbcfb6d55e6e7b12e7f0319fafb32
danfucius <danfucius@gmail.com>
2017-02-10 10:41:44 -0800

    Update home directive

--- app/directives/home/home.directive.js
+++ app/directives/home/home.directive.js
@@ -11,7 +11,7 @@
             restrict: 'E',
             controller: 'HomeContoller',
             controllerAs: 'home',
-            templateUrl: '/home.template.html'
+            templateUrl: '/app/directives/home/home.template.html'
         };
 
         return directive;
